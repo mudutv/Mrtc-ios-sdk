f37a13a509db2368ce5868f45f26e449af8c809f
张志超 <zhangzhichao@mudu.tv>
2019-05-22 14:46:44 +0800

    v1.1.2

--- build/Pods.build/Release-iphonesimulator/SocketRocket.build/Objects-normal/x86_64/SocketRocket-dummy.d
+++ build/Pods.build/Release-iphonesimulator/SocketRocket.build/Objects-normal/x86_64/SocketRocket-dummy.d
@@ -1,3 +1,3 @@
 dependencies: \
-  /var/folders/h_/0h_54byd1r9bkcmvnvv9y2hh0000gn/T/cocoapods-a2fjv5d6/Pods/Target\ Support\ Files/SocketRocket/SocketRocket-dummy.m \
-  /var/folders/h_/0h_54byd1r9bkcmvnvv9y2hh0000gn/T/cocoapods-a2fjv5d6/Pods/Target\ Support\ Files/SocketRocket/SocketRocket-prefix.pch
+  /var/folders/h_/0h_54byd1r9bkcmvnvv9y2hh0000gn/T/cocoapods-3tm48n21/Pods/Target\ Support\ Files/SocketRocket/SocketRocket-dummy.m \
+  /var/folders/h_/0h_54byd1r9bkcmvnvv9y2hh0000gn/T/cocoapods-3tm48n21/Pods/Target\ Support\ Files/SocketRocket/SocketRocket-prefix.pch
