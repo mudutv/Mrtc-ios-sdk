bb12f51ec8e18700954fc138680549f7fd719e32
张志超 <zhangzhichao@mudu.tv>
2019-05-22 14:09:42 +0800

    v1.1.2

--- build/Pods.build/Release-iphonesimulator/Pods-packager.build/Objects-normal/x86_64/Pods-packager-dummy.d
+++ build/Pods.build/Release-iphonesimulator/Pods-packager.build/Objects-normal/x86_64/Pods-packager-dummy.d
@@ -1,2 +1,2 @@
 dependencies: \
-  /var/folders/h_/0h_54byd1r9bkcmvnvv9y2hh0000gn/T/cocoapods-fezp7410/Pods/Target\ Support\ Files/Pods-packager/Pods-packager-dummy.m
+  /var/folders/h_/0h_54byd1r9bkcmvnvv9y2hh0000gn/T/cocoapods-a2fjv5d6/Pods/Target\ Support\ Files/Pods-packager/Pods-packager-dummy.m
--- build/Pods.build/Release-iphonesimulator/SocketRocket.build/Objects-normal/x86_64/SRWebSocket.d
+++ build/Pods.build/Release-iphonesimulator/SocketRocket.build/Objects-normal/x86_64/SRWebSocket.d
@@ -1,4 +1,4 @@
 dependencies: \
-  /var/folders/h_/0h_54byd1r9bkcmvnvv9y2hh0000gn/T/cocoapods-fezp7410/Pods/SocketRocket/SocketRocket/SRWebSocket.m \
-  /var/folders/h_/0h_54byd1r9bkcmvnvv9y2hh0000gn/T/cocoapods-fezp7410/Pods/Target\ Support\ Files/SocketRocket/SocketRocket-prefix.pch \
-  /var/folders/h_/0h_54byd1r9bkcmvnvv9y2hh0000gn/T/cocoapods-fezp7410/Pods/SocketRocket/SocketRocket/SRWebSocket.h
+  /var/folders/h_/0h_54byd1r9bkcmvnvv9y2hh0000gn/T/cocoapods-a2fjv5d6/Pods/SocketRocket/SocketRocket/SRWebSocket.m \
+  /var/folders/h_/0h_54byd1r9bkcmvnvv9y2hh0000gn/T/cocoapods-a2fjv5d6/Pods/Target\ Support\ Files/SocketRocket/SocketRocket-prefix.pch \
+  /var/folders/h_/0h_54byd1r9bkcmvnvv9y2hh0000gn/T/cocoapods-a2fjv5d6/Pods/SocketRocket/SocketRocket/SRWebSocket.h
